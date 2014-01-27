18350b8facb1618d51e23fabae8ebcea1c7bd9cd
Michael Wawra <michael@wawra.co.uk>
2014-01-27 21:26:15 +0000

    removing 3 trailing nanoseconds...

--- lib/rmeetup/type/event.rb
+++ lib/rmeetup/type/event.rb
@@ -37,10 +37,10 @@ module RMeetup
         self.event['rsvpcount'].to_i
       end
       def updated
-        DateTime.strptime(self.event['updated'].to_s,'%s')
+        DateTime.strptime(self.event['updated'].to_s[0...-3],'%s')
       end
       def time
-        DateTime.strptime(self.event['time'].to_s,'%s')
+        DateTime.strptime(self.event['time'].to_s[0...-3],'%s')
       end
     end
   end
